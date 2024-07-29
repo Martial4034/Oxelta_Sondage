147453d5a29f793fbe08485fcb90c36cad6f3de8
white-duke <identifiantapple.ipad6@gmail.com>
2024-07-29 16:34:57 +0200

    chore: Update email template styling and content

--- src/components/EmailTemplate.tsx
+++ src/components/EmailTemplate.tsx
@@ -1,4 +1,3 @@
-// src/app/components/EmailTemplate.tsx
 import * as React from 'react';
 
 interface EmailTemplateProps {
@@ -7,11 +6,20 @@ interface EmailTemplateProps {
 }
 
 export const SignInEmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({ url, email }) => (
-  <div style={{ fontFamily: 'Arial, sans-serif', lineHeight: 1.6 }}>
-    <h1>Bienvenue sur ton Admin PANNEL</h1>
-    <p>Merci de vous être connecté. Cliquez sur le lien ci-dessous pour vous connecter :</p>
-    <a href={url} style={{ display: 'inline-block', padding: '10px 20px', margin: '10px 0', fontSize: '18px', color: '#fff', backgroundColor: '#007bff', textDecoration: 'none' }}>Se connecter</a>
-    <p>Si vous n'avez pas demandé cette connexion, ignorez cet email.</p>
-    <p>Email: {email}</p>
+  <div style={{ fontFamily: 'Arial, sans-serif', margin: 0, padding: 0, backgroundColor: '#f4f4f4' }}>
+    <div style={{ maxWidth: '600px', margin: '0 auto', backgroundColor: '#ffffff', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)', borderRadius: '8px', overflow: 'hidden' }}>
+      <div style={{ backgroundColor: '#25A6D5', color: '#ffffff', textAlign: 'center', padding: '20px' }}>
+        <img src="https://i.imgur.com/2oXap3g.png" alt="Logo" style={{ maxWidth: '150px', marginBottom: '10px' }} />
+      </div>
+      <div style={{ padding: '20px', textAlign: 'center' }}>
+        <h1 style={{ fontSize: '24px', color: '#333333' }}>Bienvenue sur ton Admin PANNEL</h1>
+        <p style={{ fontSize: '16px', color: '#555555', lineHeight: 1.5 }}>Merci de vous être connecté. Cliquez sur le lien ci-dessous pour vous connecter :</p>
+      <div style={{ padding: '20px', textAlign: 'center', backgroundColor: '#f4f4f4' }}>
+        <a href={url} style={{ display: 'inline-block', padding: '10px 20px', margin: '10px 0', fontSize: '18px', color: '#fff', backgroundColor: '#007bff', textDecoration: 'none', borderRadius: '4px' }}>Se connecter</a>
+      </div>
+        <p style={{ fontSize: '16px', color: '#555555' }}>Si vous n&apos;avez pas demandé cette connexion, ignorez cet email.</p>
+        <p style={{ fontSize: '16px', color: '#555555' }}>Email: {email}</p>
+      </div>
+    </div>
   </div>
 );
