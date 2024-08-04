0b72c45c9b8bcea32624b5f94511de9cddf28d3d
white-duke <identifiantapple.ipad6@gmail.com>
2024-08-04 12:07:03 +0200

    chore: Update middleware to handle subdomain redirection and session check

--- middleware.ts
+++ middleware.ts
@@ -4,23 +4,23 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   const sessionCookie = request.cookies.get('next-auth.session-token') || request.cookies.get('__Secure-next-auth.session-token');
 
-  // Extract hostname and path from the request
+  // Extract hostname from the request
   const { hostname, pathname } = request.nextUrl;
 
   // Subdomain redirection logic
-  if (pathname === '/') {
-    if (hostname === 'deck.oxelta.io') {
-      // Redirect to the UK PDF viewer page
-      return NextResponse.redirect(new URL('/pdf-viewer-uk', request.url));
-    } else if (hostname === 'deck.vf.oxelta.io') {
-      // Redirect to the VF PDF viewer page
-      return NextResponse.redirect(new URL('/pdf-viewer-vf', request.url));
-    }
+  if (hostname === 'deck.vf.oxelta.io') {
+    // Redirect any path on deck.vf.oxelta.io to /pdf-viewer-vf
+    return NextResponse.redirect(new URL('/pdf-viewer-vf', 'https://sondage.oxelta.io'));
+  } else if (hostname === 'deck.oxelta.io') {
+    // Redirect any path on deck.oxelta.io to /pdf-viewer-uk
+    return NextResponse.redirect(new URL('/pdf-viewer-uk', 'https://sondage.oxelta.io'));
   }
 
-  // Session check logic for specific paths
-  if (!sessionCookie && ['/dashboard', '/api/uploadPdf'].some((path) => pathname.startsWith(path))) {
-    return NextResponse.redirect(new URL('/auth/signin', request.url));
+  // Session check logic for specific paths on sondage.oxelta.io
+  if (hostname === 'sondage.oxelta.io') {
+    if (!sessionCookie && ['/dashboard', '/api/uploadPdf'].some((path) => pathname.startsWith(path))) {
+      return NextResponse.redirect(new URL('/auth/signin', request.url));
+    }
   }
 
   // Allow other requests to pass through
@@ -28,5 +28,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/', '/dashboard/:path*', '/api/uploadPdf'],
+  matcher: ['/dashboard/:path*', '/api/uploadPdf'],
 };
