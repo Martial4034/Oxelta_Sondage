f1b48111eeeead43a53000460dc1b5a61bd001d7
white-duke <identifiantapple.ipad6@gmail.com>
2024-08-04 12:12:27 +0200

    chore: Update middleware to handle subdomain redirection and session check

--- middleware.ts
+++ middleware.ts
@@ -2,19 +2,19 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  const sessionCookie = request.cookies.get('next-auth.session-token') || request.cookies.get('__Secure-next-auth.session-token');
-
-  // Extract hostname from the request
   const { hostname, pathname } = request.nextUrl;
 
   // Subdomain redirection logic
   if (hostname === 'deck.vf.oxelta.io') {
-    // Redirect any path on deck.vf.oxelta.io to /pdf-viewer-vf
-    return NextResponse.redirect(new URL('/pdf-viewer-vf', 'https://sondage.oxelta.io'));
+    // Redirect any request from deck.vf.oxelta.io to sondage.oxelta.io/pdf-viewer-vf
+    return NextResponse.redirect('https://sondage.oxelta.io/pdf-viewer-vf');
   } else if (hostname === 'deck.oxelta.io') {
-    // Redirect any path on deck.oxelta.io to /pdf-viewer-uk
-    return NextResponse.redirect(new URL('/pdf-viewer-uk', 'https://sondage.oxelta.io'));
+    // Redirect any request from deck.oxelta.io to sondage.oxelta.io/pdf-viewer-uk
+    return NextResponse.redirect('https://sondage.oxelta.io/pdf-viewer-uk');
   }
+
+  // Extract session cookie
+  const sessionCookie = request.cookies.get('next-auth.session-token') || request.cookies.get('__Secure-next-auth.session-token');
 
   // Session check logic for specific paths on sondage.oxelta.io
   if (hostname === 'sondage.oxelta.io') {
@@ -28,5 +28,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/api/uploadPdf'],
+  matcher: ['/dashboard/:path*', '/api/uploadPdf', '/(.*)'],
 };
