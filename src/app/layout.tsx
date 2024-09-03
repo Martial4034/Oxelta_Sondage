01661252f7228ed2af0078108b9dc58922afef9e
white-duke <identifiantapple.ipad6@gmail.com>
2024-09-03 09:53:25 +0200

    chore: Update layout component to use simplified title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export async function generateMetadata({
   // Par exemple, ici on utilise une logique simplifiée :
   const hostname = typeof window !== 'undefined' ? window.location.hostname : '';
 
-  let title = 'Sondage Oxelta';
+  let title = 'Oxelta';
   let description =
     'Oxelta, the future of gaming with Web 3. Play and earn OXLT tokens with our innovative ecosystem of play-and-earn games, Web 3, NFTs, blockchain games, crypto rewards, and game economy';
 
