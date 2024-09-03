d23e016fc5c900f382962c26f6ac7bc33bbc219c
white-duke <identifiantapple.ipad6@gmail.com>
2024-09-03 10:26:18 +0200

    chore: Update middleware and layout components for subdomain redirection and dynamic metadata

--- middleware.ts
+++ middleware.ts
@@ -3,21 +3,15 @@ import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
   const { hostname, pathname } = request.nextUrl;
-  const url = request.nextUrl.clone();
 
-  // Gestion des redirections et des métadonnées dynamiques
-  if (hostname === 'deck.fr.oxelta.io') {
-    url.pathname = '/pdf-viewer-vf';
-    const response = NextResponse.rewrite(url);
-    response.headers.set('x-meta-title', 'Deck Oxelta FR');
-    response.headers.set('x-meta-description', 'Deck de Oxelta, le futur du jeu avec le Web 3. Jouez et gagnez des jetons OXLT avec notre écosystème innovant de jeux play-to-earn, Web 3, NFTs, jeux blockchain, récompenses crypto et économie de jeu');
-    return response;
-  } else if (hostname === 'deck.oxelta.io') {
-    url.pathname = '/pdf-viewer-uk';
-    const response = NextResponse.rewrite(url);
-    response.headers.set('x-meta-title', 'Deck Oxelta EN');
-    response.headers.set('x-meta-description', "Deck Oxelta, the future of gaming with Web 3. Play and earn OXLT tokens with our innovative ecosystem of play-and-earn games, Web 3, NFTs, blockchain games, crypto rewards, and game economy");
-    return response;
+  // Redirection automatique pour deck.oxelta.io vers /pdf-viewer-uk
+  if (hostname === 'deck.oxelta.io' && pathname === '/') {
+    return NextResponse.redirect(new URL('/pdf-viewer-uk', request.url));
+  }
+
+  // Redirection automatique pour deck.fr.oxelta.io vers /pdf-viewer-vf
+  if (hostname === 'deck.fr.oxelta.io' && pathname === '/') {
+    return NextResponse.redirect(new URL('/pdf-viewer-vf', request.url));
   }
 
   // Gestion de la session pour les pages protégées de sondage.oxelta.io
