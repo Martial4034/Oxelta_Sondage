b43d2044ccc1eb196176c91534847376b70f7382
white-duke <identifiantapple.ipad6@gmail.com>
2024-11-25 12:31:29 +0100

    oRAL uk update

--- src/app/oral-pdf-viewer-uk/page.tsx
+++ src/app/oral-pdf-viewer-uk/page.tsx
@@ -1,8 +1,8 @@
-import { getSignedUrlForVfSheetPdf } from '@/lib/firebaseAdmin';
+import { getSignedUrlForUkOralPdf } from '@/lib/firebaseAdmin';
 import PDFLoader from '@/components/PDFLoader';
 
 export default async function PDFViewerUKPage() {
-  const signedUrl = await getSignedUrlForVfSheetPdf();
+  const signedUrl = await getSignedUrlForUkOralPdf();
 
   return <PDFLoader signedUrl={signedUrl} />;
 }
--- src/app/oral-pdf-viewer-vf/page.tsx
+++ src/app/oral-pdf-viewer-vf/page.tsx
@@ -1,8 +1,8 @@
-import { getSignedUrlForUkOralPdf } from '@/lib/firebaseAdmin';
+import { getSignedUrlForVfOralPdf } from '@/lib/firebaseAdmin';
 import PDFLoader from '@/components/PDFLoader';
 
 export default async function PDFViewerVFPage() {
-  const signedUrl = await getSignedUrlForUkOralPdf();
+  const signedUrl = await getSignedUrlForVfOralPdf();
 
   return <PDFLoader signedUrl={signedUrl} />;
 }
